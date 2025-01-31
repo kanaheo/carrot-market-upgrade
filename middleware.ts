63632cc5ff6495560276ba59d5e94ad08fe31ec6
KyoungMin HEO <stalvede0421@gmail.com>
2025-01-31 15:31:14 +0900

    middleware end

--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,32 @@
-import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
 import getSession from "./lib/session";
 
+interface Routes {
+  [key: string]: boolean;
+}
+
+const publicOnlyUrls: Routes = {
+  "/": true,
+  "/login": true,
+  "/sms": true,
+  "/create-account": true,
+};
+
 export async function middleware(request: NextRequest) {
-  const pathname = request.nextUrl.pathname;
-  if (pathname === "/") {
-    const response = NextResponse.next();
-    response.cookies.set("middleware-cookie", "hello!");
-    return response;
+  const session = await getSession();
+  const exists = publicOnlyUrls[request.nextUrl.pathname];
+  // fail login
+  if (!session.id) {
+    if (!exists) {
+      return NextResponse.redirect(new URL("/", request.url));
+    }
+  } else {
+    if (exists) {
+      return NextResponse.redirect(new URL("/products", request.url));
+    }
   }
 }
 
 export const config = {
-  matcher: ["/", "/profile", "create-account", "/user/:path*"],
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)"],
 };
