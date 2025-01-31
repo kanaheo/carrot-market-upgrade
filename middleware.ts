0cfe475178a906c80e15c11cd19585a05b4691eb
KyoungMin HEO <stalvede0421@gmail.com>
2025-01-31 14:44:20 +0900

    middleware add config

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,16 @@
 import { cookies } from "next/headers";
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import getSession from "./lib/session";
 
 export async function middleware(request: NextRequest) {
-  const session = await getSession();
-  console.log(session);
-  console.log("Hi im middleware");
+  const pathname = request.nextUrl.pathname;
+  if (pathname === "/") {
+    const response = NextResponse.next();
+    response.cookies.set("middleware-cookie", "hello!");
+    return response;
+  }
 }
+
+export const config = {
+  matcher: ["/", "/profile", "create-account", "/user/:path*"],
+};
