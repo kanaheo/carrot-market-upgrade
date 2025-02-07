a4402d78601fd71233a48b79cc1215c6f5d652f6
KyoungMin HEO <stalvede0421@gmail.com>
2025-02-07 10:35:06 +0900

    sms auth & token end

--- app/sms/actions.ts
+++ app/sms/actions.ts
@@ -1,49 +1,126 @@
-'use server';
+"use server";
 
-import { error } from 'console';
-import { redirect } from 'next/navigation';
-import validator from 'validator';
-import { z } from 'zod';
+import db from "@/lib/db";
+import { error } from "console";
+import { redirect } from "next/navigation";
+import validator from "validator";
+import { z } from "zod";
+import crypto from "crypto";
+import getSession from "@/lib/session";
+import twilio from "twilio";
 
 interface ActionState {
   token: boolean;
 }
 
+async function getToken() {
+  const token = crypto.randomInt(100000, 999999).toString();
+  const exists = await db.sMSToken.findUnique({
+    where: {
+      token,
+    },
+    select: {
+      id: true,
+    },
+  });
+  if (exists) {
+    return getToken();
+  } else {
+    return token;
+  }
+}
+
 const phoneSchema = z
   .string()
   .trim()
-  .refine(
-    (phone) => validator.isMobilePhone(phone, 'ko-KR'),
-    'Wrong phone format'
-  );
+  .refine((phone) => validator.isMobilePhone(phone, ["ko-KR", "ja-JP"]), "Wrong phone format");
 
-// coerce는 강제로 타입 지정해주기
-const tokenSchema = z.coerce.number().min(100000).max(999999);
+async function tokenExists(token: number) {
+  const exists = await db.sMSToken.findUnique({
+    where: {
+      token: token.toString(),
+    },
+    select: {
+      id: true,
+    },
+  });
+  return Boolean(exists);
+}
+
+const tokenSchema = z.coerce.number().min(100000).max(999999).refine(tokenExists, "This token does not exist.");
 
 export async function smsLogIn(prevState: ActionState, formData: FormData) {
-  const phone = formData.get('phone');
-  const token = formData.get('token');
+  const phone = formData.get("phone");
+  const token = formData.get("token");
   if (!prevState.token) {
-    const result = phoneSchema.safeParse(phone);
-    if (!result.success) {
+    const resultPhone = phoneSchema.safeParse(phone);
+    if (!resultPhone.success) {
       return {
         token: false,
-        error: result.error.flatten(),
+        error: resultPhone.error.flatten(),
       };
     } else {
+      await db.sMSToken.deleteMany({
+        where: {
+          user: {
+            phone: resultPhone.data,
+          },
+        },
+      });
+      const token = await getToken();
+      await db.sMSToken.create({
+        data: {
+          token,
+          user: {
+            connectOrCreate: {
+              where: {
+                phone: resultPhone.data,
+              },
+              create: {
+                username: crypto.randomBytes(10).toString("hex"),
+                phone: resultPhone.data,
+              },
+            },
+          },
+        },
+      });
+      // send the token using twilio
+      const client = twilio(process.env.TWILIO_ACCOUNT_SID!, process.env.TWILIO_AUTH_TOKEN!);
+      await client.messages.create({
+        body: `Your Carrot verification code is : ${token}`,
+        from: process.env.TWILIO_PHONE_NUMBER!,
+        to: process.env.MY_PHONE_NUMBER!, // resultPhone.data but test my phone number
+      });
       return {
         token: true,
       };
     }
   } else {
-    const result = tokenSchema.safeParse(token);
+    const result = await tokenSchema.spa(token);
     if (!result.success) {
       return {
         token: true,
         error: result.error.flatten(),
       };
     } else {
-      redirect('/');
+      const token = await db.sMSToken.findUnique({
+        where: {
+          token: result.data.toString(),
+        },
+        select: {
+          id: true,
+          userId: true,
+        },
+      });
+      const session = await getSession();
+      session.id = token!.userId;
+      await session.save();
+      await db.sMSToken.delete({
+        where: {
+          id: token!.id,
+        },
+      });
+      redirect("/profile");
     }
   }
 }
